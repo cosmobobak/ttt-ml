ce750c7e5ddbe6409069e2937c313723e2a732c5
cosmobobak <cosmobobak@gmail.com>
2021-09-27 17:58:46 +0100

    more, correcter, tests!

--- Tests.py
+++ Tests.py
@@ -1,15 +1,14 @@
 from State import State
+from Oracle import oracle_value
 import numpy as np
 
 def test_move_make():
     board = State()
     board.push(4)
-    expected = np.array([
-        np.array([0, 0, 0, 0, 1, 0, 0, 0, 0], dtype=int),
-        np.array([0, 0, 0, 0, 0, 0, 0, 0, 0], dtype=int),
-    ])
+    expected = np.zeros((2, 3, 3), dtype=int)
+    expected[0, 1, 1] = 1
 
-    assert np.array_equal(board.vectorise(), expected)
+    assert np.array_equal(board.vectorise(), expected), f"Error: {board.vectorise()} != {expected}"
 
 
 def test_make_unmake():
@@ -25,14 +24,35 @@ def test_make_unmake():
     for move in moves:
         board.pop()
 
-    expected = np.array(
-        [
-            np.zeros(9, dtype=int),
-            np.zeros(9, dtype=int)
-        ]
-    )
+    expected = np.zeros((2, 3, 3), dtype=int)
 
-    assert np.array_equal(board.vectorise(), expected)
+    assert np.array_equal(board.vectorise(), expected), f"Error: {board.vectorise()} != {expected}"
+
+def test_oracle():
+    if not oracle_value(State()) == 0:
+        print("Error: value(State()) != 0")
+        print(f"{oracle_value(State()) = }")
+        print(State())
+        exit(1)
+
+    b = State()
+    b.play(0)
+    b.play(1)
+    if not oracle_value(b) == 1:
+        print("Error: value(b) != 1")
+        print(f"{oracle_value(b) = }")
+        print(b)
+        exit(1)
+
+    b.play(2)
+    b.play(4)
+    b.play(3)
+
+    if not oracle_value(b) == -1:
+        print("Error: value(b) != -1")
+        print(f"{oracle_value(b) = }")
+        print(b)
+        exit(1)
 
 if __name__ == "__main__":
     test_move_make()
