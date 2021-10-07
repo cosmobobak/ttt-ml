f91c4d7b0971d1b23a51f71ded0782bfd1c1ae24
cosmobobak <cosmobobak@gmail.com>
2021-10-07 15:46:44 +0100

    added testing for oracle

--- Oracle.py
+++ Oracle.py
@@ -1,14 +1,11 @@
 from State import State
-from copy import copy
-
 
 def oracle_value(state: State) -> float:
-    mutator = copy(state)
+    mutator = state.clone()
     return negamax(mutator, a=float('-inf'), b=float('inf'), depth=10) * state.get_turn()
 
-
 def negamax(state: State, a: float, b: float, depth: int) -> float:
-    if depth == 0 or state.is_game_over():
+    if depth == 0 or state.is_terminal():
         return state.evaluate() * state.get_turn()
     score = 0
     for move in state.legal_moves():
@@ -19,3 +16,47 @@ def negamax(state: State, a: float, b: float, depth: int) -> float:
         if a >= b:
             break
     return a
+
+if __name__ == '__main__':
+    state = State()
+    assert oracle_value(state) == 0, "Incorrect root eval."
+    print(state)
+    print(f"{oracle_value(state)=}")
+    state.play(0)
+    state.play(1)
+    assert oracle_value(state) == 1, "Incorrect forced X win eval."
+    print(state)
+    print(f"{oracle_value(state)=}")
+    state.play(4)
+    state.play(8)
+    state.play(6)
+    state.play(2)
+    assert oracle_value(state) == 1, "Incorrect immediate X win eval."
+    print(state)
+    print(f"{oracle_value(state)=}")
+    state.play(3)
+    assert oracle_value(state) == 1, "Incorrect gameover X win eval."
+    print(state)
+    print(f"{oracle_value(state)=}")
+
+    state = State()
+    state.play(1)
+    state.play(4)
+    state.play(7)
+    assert oracle_value(state) == -1, "Incorrect forced O win eval."
+    print(state)
+    print(f"{oracle_value(state)=}")
+    state.play(2)
+    state.play(6)
+    state.play(8)
+    assert oracle_value(state) == -1, "Incorrect forced O win eval."
+    print(state)
+    print(f"{oracle_value(state)=}")
+    state.play(0)
+    assert oracle_value(state) == -1, "Incorrect immediate O win eval."
+    print(state)
+    print(f"{oracle_value(state)=}")
+    state.play(5)
+    assert oracle_value(state) == -1, "Incorrect gameover O win eval."
+    print(state)
+    print(f"{oracle_value(state)=}")
