6d2391b75007d566dd31eedb5854de9d45776d20
cosmobobak <cosmobobak@gmail.com>
2021-11-22 12:40:21 +0000

    experimental model arch

--- az_method/InitNetwork.py
+++ az_method/InitNetwork.py
@@ -1,76 +1,89 @@
 import os
-
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-from keras.regularizers import l2
-from tensorflow.keras.layers import Conv2D, Flatten, Dense, BatchNormalization, Input, concatenate, Reshape, Add
-from tensorflow.keras.models import Model
 import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, BatchNormalization, Input, concatenate, Reshape, Add, GlobalAveragePooling2D, multiply
+from keras.regularizers import l2
+import tensorflow.keras.backend as K
 
 TTT_DIM = (3, 3, 2)
 TTT_ACTION_SPACE = 9
 C4_DIM = (6, 7, 2)
 C4_ACTION_SPACE = 7
 
+FILTERS = 128
+KERNEL_SIZE = (3, 3)
+
+
+def squeeze_excite_block(tensor, ratio=16):
+    init = tensor
+    se_shape = (1, 1, FILTERS)
+
+    se = GlobalAveragePooling2D()(init)
+    se = Reshape(se_shape)(se)
+    se = Dense(FILTERS // ratio, activation='relu',
+               kernel_initializer='he_normal', use_bias=False)(se)
+    se = Dense(FILTERS, activation='sigmoid',
+               kernel_initializer='he_normal', use_bias=False)(se)
+
+    x = multiply([init, se])
+    return x
+
+def resnet_block(input_layer):
+    x = Conv2D(filters=FILTERS, kernel_size=KERNEL_SIZE, padding='same', strides=1,
+               activation='relu')(input_layer)
+    x = Conv2D(filters=FILTERS, kernel_size=KERNEL_SIZE, padding='same', strides=1,
+               activation='relu')(x)
+    x = squeeze_excite_block(x)
+    x = Add()([x, input_layer])
+    return x
+
 input_layer = Input(
     shape=C4_DIM, name="input")
 
-x = Conv2D(
-    filters=256,
-    kernel_size=3,
-    strides=1,
-    padding="same",
-    activation="relu",
-    name="Conv0",
-    input_shape=C4_DIM,
-)(input_layer)
-x = Conv2D(
-    filters=256,
-    kernel_size=3,
-    strides=1,
-    padding="same",
-    activation="relu",
-    name="Conv1",
-)(x)
-preconv = x
-postconv = Conv2D(
-    filters=256,
-    kernel_size=3,
-    strides=1,
-    padding="same",
-    activation="relu",
-    name="Conv2",
-)(preconv)
-preconv = Add()([preconv, postconv])
-postconv = Conv2D(
-    filters=256,
-    kernel_size=3,
-    strides=1,
-    padding="same",
-    activation="relu",
-    name="Conv3",
-)(preconv)
-preconv = Add()([preconv, postconv])
-postconv = Conv2D(
-    filters=256,
-    kernel_size=3,
-    strides=1,
-    padding="same",
-    activation="relu",
-    name="Conv4",
-)(preconv)
-preconv = Add()([preconv, postconv])
-x = preconv
-x = Flatten()(x)
+x = Conv2D(filters=FILTERS, kernel_size=KERNEL_SIZE, padding='same', strides=1,
+           activation='relu', input_shape=C4_DIM)(input_layer)
+
+x = resnet_block(x)
+x = resnet_block(x)
+x = resnet_block(x)
+x = resnet_block(x)
+x = resnet_block(x)
+
+x = resnet_block(x)
+x = resnet_block(x)
+x = resnet_block(x)
+x = resnet_block(x)
+x = resnet_block(x)
 
 policy_head = x
-policy_head = Dense(256, activation="relu")(policy_head)
-policy_head = Dense(28, activation="relu")(policy_head)
+policy_head = Conv2D(
+    filters=32, 
+    kernel_size=(1, 1), 
+    padding='valid', 
+    strides=1,
+    activation='relu')(policy_head)
+policy_head = Flatten()(policy_head)
 
 policy_out = Dense(C4_ACTION_SPACE, activation="softmax",
                    name="policy_head")(policy_head)
 
 value_head = x
-value_head = Dense(64, activation="relu")(value_head)
+value_head = Conv2D(
+    filters=32,
+    kernel_size=(1, 1),
+    padding='same',
+    strides=1,
+    activation='relu')(value_head)
+# then we do a convolution to 1x1x128 vector
+value_head = Conv2D(
+    filters=128,
+    kernel_size=C4_DIM[0:2],
+    padding='valid',
+    strides=1,
+    activation='relu')(value_head)
+# then we flatten it
+value_head = Flatten()(value_head)
 
 value_out = Dense(1, activation="tanh", name="value_head")(value_head)
 
