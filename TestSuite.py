691c7c79bb09b6f2da26724b9e9b159e4a689120
cosmobobak <cosmobobak@gmail.com>
2021-11-30 16:38:05 +0000

    inconsequential

--- TestSuite.py
+++ TestSuite.py
@@ -12,24 +12,19 @@ sup_model = typing.cast(
     Model, keras.models.load_model("supervised_models/supervised_model.h5"))
 random_model = typing.cast(
     Model, keras.models.load_model("az_models/random_model.keras"))
-az_model35 = typing.cast(
-    Model, keras.models.load_model("az_models/model_it35.keras"))
-az_model40 = typing.cast(
-    Model, keras.models.load_model("az_models/model_it40.keras"))
-az_model41 = typing.cast(
-    Model, keras.models.load_model("az_models/model_it41.keras"))
-az_model42 = typing.cast(
-    Model, keras.models.load_model("az_models/model_it42.keras"))
-az_model43 = typing.cast(
-    Model, keras.models.load_model("az_models/model_it43.keras"))
-az_model44 = typing.cast(
-    Model, keras.models.load_model("az_models/model_it44.keras"))
-az_model45 = typing.cast(
-    Model, keras.models.load_model("az_models/model_it45.keras"))
+az_model1 = typing.cast(
+    Model, keras.models.load_model("az_models/model_1.keras"))
+az_model2 = typing.cast(
+    Model, keras.models.load_model("az_models/model_2.keras"))
+az_model3 = typing.cast(
+    Model, keras.models.load_model("az_models/model_3.keras"))
+
 
 AGENTS = [
     RandomAgent(),
-    AZAgent(az_model45, "model45", 100),
+    AZAgent(az_model1, "model1", 10),
+    AZAgent(az_model2, "model2", 10),
+    AZAgent(az_model3, "model3", 10),
 ]
 
 all_pairs = [(a1, a2) for a1 in AGENTS for a2 in AGENTS if a1 != a2]
@@ -40,7 +35,7 @@ for a1, a2 in all_pairs:
 
 for a1, a2 in matchups:
     print(f"{a1} vs {a2}")
-    results = [ play_game(a1, a2, i & 1 == 0, game=C4State) for i in tqdm(range(100)) ]
+    results = [ play_game(a1, a2, i & 1 == 0, game=C4State) for i in tqdm(range(50)) ]
     x_wins = sum(1 for result in results if result == 1)
     o_wins = sum(1 for result in results if result == -1)
     draws =  sum(1 for result in results if result == 0)
